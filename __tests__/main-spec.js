6bd243b12fb43ef94427a60fbb2f6dd3b0132e54
unknown <1012151538@qq.comgit config --global user.name >
2019-08-22 12:05:15 +0800

    new

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,29 @@
-const add = require('../main');
+const {add,countProducts,fetchProducts}= require('../main');
 
 it ('should add two numbers', () => {
     expect(add(2, 3)).toBe(5);
 });
+
+it ('should count codes', () => {
+   //given
+   const codes=['0001', '0003', '0005', '0003'];
+   //when
+   var countCodes=countProducts(codes);
+   //then
+
+    expect(countCodes[0].count).toBe(1);
+});
+
+it ('should fetch  product', () => {
+    //given
+    const db=[
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0002", "name" : "Diet Coke", "price": 4}
+    ]
+    const code='0001';
+    //when
+    var product=fetchProducts(code,db);
+    //then
+ 
+     expect(product.price).toBe(3);
+ });
--- main.js
+++ main.js
@@ -1,5 +1,47 @@
-function add (left, right) {
+function add(left, right) {
     return left + right;
 }
 
-module.exports = add;
+function countProducts(codes) {
+    var myMap = new Map();
+    for (let i = 0; i < codes.length; i++) {
+        var code = codes[i];
+        if (!myMap.has(codes[i])) {
+            var item = {
+                code: code, count: 1
+            };
+            myMap.set(code, item);
+            // myMap.get(codes[i])+1;
+        }
+        else {
+            var items = myMap.get(code);
+            console.log(items);
+            items.count++;
+            myMap.set(code, items)
+        }
+
+    }
+
+    let results = [];
+    myMap.forEach(function (item)
+     {
+        results.push(item)
+     }
+
+    )
+    return results;
+}
+
+     function fetchProducts(code,db)
+     {
+         for (let index = 0; index < db.length; index++) {
+             if(db[index].id==code)
+             return {
+                price:db[index].price,
+                name:db[index].name
+                    }
+             
+         }
+     }
+module.exports = { add, countProducts,fetchProducts };
+
